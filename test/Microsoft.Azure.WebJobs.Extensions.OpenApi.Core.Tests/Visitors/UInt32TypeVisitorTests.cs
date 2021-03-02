dc0bbfbed815a13d411ab6ed5daee82857678959
Steve Ashman <sashman@isolvedhcm.com>
2021-03-01 21:22:11 -0500

    Add visitors for unsigned integer types

---
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests/Visitors/UInt32TypeVisitorTests.cs
@@ -0,0 +1,137 @@
+using FluentAssertions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Visitors;
+using Microsoft.OpenApi.Any;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Visitors
+{
+    [TestClass]
+    public class UInt32TypeVisitorTests
+    {
+        private VisitorCollection _visitorCollection;
+        private IVisitor _visitor;
+        private NamingStrategy _strategy;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this._visitorCollection = new VisitorCollection();
+            this._visitor = new UInt32TypeVisitor(this._visitorCollection);
+            this._strategy = new CamelCaseNamingStrategy();
+        }
+
+        [DataTestMethod]
+        [DataRow(typeof(uint), false)]
+        public void Given_Type_When_IsNavigatable_Invoked_Then_It_Should_Return_Result(Type type, bool expected)
+        {
+            var result = this._visitor.IsNavigatable(type);
+
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(typeof(uint), true)]
+        [DataRow(typeof(int), false)]
+        [DataRow(typeof(string), false)]
+        public void Given_Type_When_IsVisitable_Invoked_Then_It_Should_Return_Result(Type type, bool expected)
+        {
+            var result = this._visitor.IsVisitable(type);
+
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(typeof(uint), true)]
+        [DataRow(typeof(int), false)]
+        [DataRow(typeof(string), false)]
+        public void Given_Type_When_IsParameterVisitable_Invoked_Then_It_Should_Return_Result(Type type, bool expected)
+        {
+            var result = this._visitor.IsParameterVisitable(type);
+
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(typeof(uint), true)]
+        [DataRow(typeof(int), false)]
+        [DataRow(typeof(string), false)]
+        public void Given_Type_When_IsPayloadVisitable_Invoked_Then_It_Should_Return_Result(Type type, bool expected)
+        {
+            var result = this._visitor.IsPayloadVisitable(type);
+
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow("integer", null)]
+        public void Given_Type_When_Visit_Invoked_Then_It_Should_Return_Result(string dataType, string dataFormat)
+        {
+            var name = "hello";
+            var acceptor = new OpenApiSchemaAcceptor();
+            var type = new KeyValuePair<string, Type>(name, typeof(uint));
+
+            this._visitor.Visit(acceptor, type, this._strategy);
+
+            acceptor.Schemas.Should().ContainKey(name);
+            acceptor.Schemas[name].Type.Should().Be(dataType);
+            acceptor.Schemas[name].Format.Should().Be(dataFormat);
+        }
+
+        [DataTestMethod]
+        [DataRow("hello", "lorem ipsum")]
+        public void Given_OpenApiPropertyDescriptionAttribute_When_Visit_Invoked_Then_It_Should_Return_Result(string name, string description)
+        {
+            var acceptor = new OpenApiSchemaAcceptor();
+            var type = new KeyValuePair<string, Type>(name, typeof(uint));
+            var attribute = new OpenApiPropertyDescriptionAttribute(description);
+
+            this._visitor.Visit(acceptor, type, this._strategy, attribute);
+
+            acceptor.Schemas[name].Description.Should().Be(description);
+        }
+
+        [DataTestMethod]
+        [DataRow("hello", OpenApiVisibilityType.Advanced)]
+        [DataRow("hello", OpenApiVisibilityType.Important)]
+        [DataRow("hello", OpenApiVisibilityType.Internal)]
+        public void Given_OpenApiSchemaVisibilityAttribute_When_Visit_Invoked_Then_It_Should_Return_Result(string name, OpenApiVisibilityType visibility)
+        {
+            var acceptor = new OpenApiSchemaAcceptor();
+            var type = new KeyValuePair<string, Type>(name, typeof(uint));
+            var attribute = new OpenApiSchemaVisibilityAttribute(visibility);
+
+            this._visitor.Visit(acceptor, type, this._strategy, attribute);
+
+            acceptor.Schemas[name].Extensions.Should().ContainKey("x-ms-visibility");
+            acceptor.Schemas[name].Extensions["x-ms-visibility"].Should().BeOfType<OpenApiString>();
+            (acceptor.Schemas[name].Extensions["x-ms-visibility"] as OpenApiString).Value.Should().Be(visibility.ToDisplayName(this._strategy));
+        }
+
+        [DataTestMethod]
+        [DataRow("integer", null)]
+        public void Given_Type_When_ParameterVisit_Invoked_Then_It_Should_Return_Result(string dataType, string dataFormat)
+        {
+            var result = this._visitor.ParameterVisit(typeof(uint), this._strategy);
+
+            result.Type.Should().Be(dataType);
+            result.Format.Should().Be(dataFormat);
+        }
+
+        [DataTestMethod]
+        [DataRow("integer", null)]
+        public void Given_Type_When_PayloadVisit_Invoked_Then_It_Should_Return_Result(string dataType, string dataFormat)
+        {
+            var result = this._visitor.PayloadVisit(typeof(uint), this._strategy);
+
+            result.Type.Should().Be(dataType);
+            result.Format.Should().Be(dataFormat);
+        }
+    }
+}
