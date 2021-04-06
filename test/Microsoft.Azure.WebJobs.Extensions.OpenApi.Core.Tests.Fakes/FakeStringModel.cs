cf9ac5eb81cbb020f46cb655a3b6e646b21afdd7
Steve Ashman <steve@ash.mn>
2021-04-05 22:08:19 -0400

    Add unit test to validate schema output from fake set of functions with Request/Response attributes

---
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Fakes/FakeStringModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Fakes
+{
+    public class FakeStringModel
+    {
+        public string StringValue { get; set; }
+
+        public Uri UriValue { get; set; }
+    }
+}
