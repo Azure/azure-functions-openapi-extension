535da341c050d37087468ae662a5624f0596dfec
Justin Yoo <justin.yoojh@gmail.com>
2021-04-21 13:50:59 +0900

    Fix the executing function app assembly location

--- test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests/OpenApiHttpTriggerContextTests.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests/OpenApiHttpTriggerContextTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 using FluentAssertions;
 
@@ -16,13 +17,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests
     {
         [DataTestMethod]
         [DataRow(typeof(OpenApiHttpTriggerContextTests))]
-        public void Given_Type_When_Initiated_Then_It_Should_Return_ApplicationAssemblyWithGivenType(Type type)
+        public async Task Given_Type_When_Initiated_Then_It_Should_Return_ApplicationAssemblyWithGivenType(Type type)
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var assembly = context.SetApplicationAssembly(location, false)
-                                  .ApplicationAssembly;
+            var assembly = (await context.SetApplicationAssemblyAsync(location, false))
+                                         .ApplicationAssembly;
 
             var ti = type.GetTypeInfo();
 
@@ -34,13 +35,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests
         [DataRow(typeof(OpenApiHttpTriggerContext))]
         [DataRow(typeof(OpenApiTriggerFunctionProvider))]
         [DataRow(typeof(ISwaggerUI))]
-        public void Given_Type_When_Initiated_Then_It_Should_NotReturn_ApplicationAssemblyWithGivenType(Type type)
+        public async Task Given_Type_When_Initiated_Then_It_Should_NotReturn_ApplicationAssemblyWithGivenType(Type type)
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var assembly = context.SetApplicationAssembly(location, false)
-                                  .ApplicationAssembly;
+            var assembly = (await context.SetApplicationAssemblyAsync(location, false))
+                                         .ApplicationAssembly;
 
             var ti = type.GetTypeInfo();
 
@@ -48,52 +49,52 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests
         }
 
         [TestMethod]
-        public void Given_Type_When_Initiated_Then_It_Should_Return_PackageAssembly()
+        public async Task Given_Type_When_Initiated_Then_It_Should_Return_PackageAssembly()
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var assembly = context.SetApplicationAssembly(location, false)
-                                  .PackageAssembly;
+            var assembly = (await context.SetApplicationAssemblyAsync(location, false))
+                                         .PackageAssembly;
 
             assembly.DefinedTypes.Should().Contain(typeof(ISwaggerUI).GetTypeInfo());
             assembly.DefinedTypes.Should().NotContain(typeof(IOpenApiHttpTriggerContext).GetTypeInfo());
         }
 
         [TestMethod]
-        public void Given_Type_When_Initiated_Then_It_Should_Return_OpenApiConfigurationOptions()
+        public async Task Given_Type_When_Initiated_Then_It_Should_Return_OpenApiConfigurationOptions()
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var options = context.SetApplicationAssembly(location, false)
-                                 .OpenApiConfigurationOptions;
+            var options = (await context.SetApplicationAssemblyAsync(location, false))
+                                        .OpenApiConfigurationOptions;
 
             options.Info.Version.Should().Be("1.0.0");
             options.Servers.Count.Should().Be(0);
         }
 
         [TestMethod]
-        public void Given_Type_When_Initiated_Then_It_Should_Return_OpenApiCustomUIOptions()
+        public async Task Given_Type_When_Initiated_Then_It_Should_Return_OpenApiCustomUIOptions()
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var options = context.SetApplicationAssembly(location, false)
-                                 .OpenApiCustomUIOptions;
+            var options = (await context.SetApplicationAssemblyAsync(location, false))
+                                        .OpenApiCustomUIOptions;
 
             options.CustomStylesheetPath.Should().Be("dist.custom.css");
             options.CustomJavaScriptPath.Should().Be("dist.custom.js");
         }
 
         [TestMethod]
-        public void Given_Type_When_Initiated_Then_It_Should_Return_HttpSettings()
+        public async Task Given_Type_When_Initiated_Then_It_Should_Return_HttpSettings()
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var settings = context.SetApplicationAssembly(location, false)
-                                  .HttpSettings;
+            var settings = (await context.SetApplicationAssemblyAsync(location, false))
+                                         .HttpSettings;
 
             settings.RoutePrefix.Should().Be("api");
 
@@ -102,13 +103,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests
         [DataTestMethod]
         [DataRow("v2", OpenApiSpecVersion.OpenApi2_0)]
         [DataRow("v3", OpenApiSpecVersion.OpenApi3_0)]
-        public void Given_Type_When_GetOpenApiSpecVersion_Invoked_Then_It_Should_Return_Result(string version, OpenApiSpecVersion expected)
+        public async Task Given_Type_When_GetOpenApiSpecVersion_Invoked_Then_It_Should_Return_Result(string version, OpenApiSpecVersion expected)
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var result = context.SetApplicationAssembly(location, false)
-                                .GetOpenApiSpecVersion(version);
+            var result = (await context.SetApplicationAssemblyAsync(location, false))
+                                       .GetOpenApiSpecVersion(version);
 
             result.Should().Be(expected);
         }
@@ -116,13 +117,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests
         [DataTestMethod]
         [DataRow("yaml", OpenApiFormat.Yaml)]
         [DataRow("json", OpenApiFormat.Json)]
-        public void Given_Type_When_GetOpenApiSpecVersion_Invoked_Then_It_Should_Return_Result(string format, OpenApiFormat expected)
+        public async Task Given_Type_When_GetOpenApiSpecVersion_Invoked_Then_It_Should_Return_Result(string format, OpenApiFormat expected)
         {
             var location = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName;
             var context = new OpenApiHttpTriggerContext();
 
-            var result = context.SetApplicationAssembly(location, false)
-                                .GetOpenApiFormat(format);
+            var result = (await context.SetApplicationAssemblyAsync(location, false))
+                                       .GetOpenApiFormat(format);
 
             result.Should().Be(expected);
         }
