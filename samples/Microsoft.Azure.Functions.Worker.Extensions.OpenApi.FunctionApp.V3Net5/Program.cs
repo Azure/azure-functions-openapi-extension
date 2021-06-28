36225134dd18f03c3f11b7b6d8d11d2545438051
Justin Yoo <justin.yoojh@gmail.com>
2021-06-28 16:57:29 +0900

    Update .NET 5 Functions example

--- samples/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.V3Net5/Program.cs
+++ samples/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.V3Net5/Program.cs
@@ -1,38 +1,18 @@
 using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.V3Net5
 {
     public class Program
     {
-        private const string OpenApiSettingsKey = "OpenApi";
-
         public static void Main()
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
                 .ConfigureOpenApi()
-                .ConfigureServices(services =>
-                 {
-                     services.AddTransient<ISample, Sample>();
-                 })
                 .Build();
 
             host.Run();
-        }
-    }
-
-    public interface ISample
-    {
-        string GetValue();
-    }
-
-    public class Sample : ISample
-    {
-        public string GetValue()
-        {
-            return "hello world";
         }
     }
 }
