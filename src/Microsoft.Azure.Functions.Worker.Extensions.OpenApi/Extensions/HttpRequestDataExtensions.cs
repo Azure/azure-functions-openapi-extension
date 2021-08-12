60b122c7bc9308e2c0ff80a9b79a22168c9791ae
Justin Yoo <justin.yoojh@gmail.com>
2021-08-12 23:34:57 +0900

    Add HttpRequestDataExtensions

---
+++ src/Microsoft.Azure.Functions.Worker.Extensions.OpenApi/Extensions/HttpRequestDataExtensions.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
+using Microsoft.Extensions.Primitives;
+
+namespace Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions
+{
+    /// <summary>
+    /// This represents the extension entity for <see cref="HttpRequestData"/>.
+    /// </summary>
+    public static class HttpRequestDataExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="QueryCollection"/> instance from the <see cref="HttpRequestData"/>.
+        /// </summary>
+        /// <param name="req"><see cref="HttpRequestData"/> instance.</param>
+        /// <returns>Returns <see cref="QueryCollection"/> instance.</returns>
+        public static IQueryCollection Queries(this HttpRequestData req)
+        {
+            var queries = QueryHelpers.ParseNullableQuery(req.Url.Query);
+            if (queries.IsNullOrDefault())
+            {
+                queries = new Dictionary<string, StringValues>();
+            }
+
+            return new QueryCollection(queries);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="StringValues"/> object from the <see cref="HttpRequestData"/>.
+        /// </summary>
+        /// <param name="req"><see cref="HttpRequestData"/> instance.</param>
+        /// <param name="key">Querystring key.</param>
+        /// <returns>Returns <see cref="StringValues"/> object.</returns>
+        public static StringValues Query(this HttpRequestData req, string key)
+        {
+            var queries = Queries(req);
+            var value = queries.ContainsKey(key) ? queries[key] : new StringValues(default(string));
+
+            return value;
+        }
+    }
+}
