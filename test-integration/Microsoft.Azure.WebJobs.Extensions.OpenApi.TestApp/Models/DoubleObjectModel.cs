9ab11e9172706efcca27572d5119d36d18a2e07f
sonia-comp <sonia.comp@kakaoenterprise.com>
2021-09-12 00:09:42 +0900

    Add DoubleObject to test-integration (#143)

---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Models/DoubleObjectModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp.Models
+{
+    public class DoubleTypeObjectModel
+    {
+        public double Value { get; set; }
+    }
+}
