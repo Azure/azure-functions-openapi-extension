3dd1401f4ef223e41fc8370035fe8b9650ac3aa9
deserve82 <inth8204@gmail.com>
2021-10-03 11:32:41 +0900

    Add integration testing for UInt64 Object Type (#143)
    this is integration tests for UInt64 visitor type.

---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Models/UInt64ObjectModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp.Models
+{
+    public class UInt64ObjectModel
+    {
+        public UInt64 Value { get; set; }
+    }
+}
