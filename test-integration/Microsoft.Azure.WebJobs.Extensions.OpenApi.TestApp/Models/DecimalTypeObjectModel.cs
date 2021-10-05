72c92279e53da89f430c9bb4f70474813e1d1690
DoHoon Kim <minbumm@gmail.com>
2021-10-05 11:53:41 +0900

    rewrite - Add DecimalTypeObject to test-integration (#143) #237

---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Models/DecimalTypeObjectModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp.Models
+{
+    public class DecimalTypeObjectModel
+    {
+        public decimal DecimalValue { get; set; }
+    }
+}
---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Post_ApplicationJson_DecimalType_HttpTrigger.cs
@@ -0,0 +1,29 @@
+using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp.Models;
+using Microsoft.Extensions.Logging;
+
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp
+{
+    public class Post_ApplicationJson_DecimalType_HttpTrigger
+    {
+        [FunctionName(nameof(Post_ApplicationJson_DecimalType_HttpTrigger))]
+        [OpenApiOperation(operationId: nameof(Post_ApplicationJson_DecimalType_HttpTrigger.Post_ApplicationJson_DecimalType), tags: new[] { "demical" })]
+        [OpenApiRequestBody(contentType: "text/plain", bodyType: typeof(double), Required = true, Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(DecimalTypeObjectModel), Description = "The OK response")]
+        public static async Task<IActionResult> Post_ApplicationJson_DecimalType(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "POST", Route = "post-applicationjson-decimal")] HttpRequest req,
+            ILogger log)
+        {
+            var result = new OkResult();
+
+            return await Task.FromResult(result).ConfigureAwait(false);
+        }
+    }
+}
