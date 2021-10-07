d5a77143684af56c55bc67f3085d26f7f24753fc
[haileymoon] <[moongirl1148@naver.com]>
2021-10-07 18:41:45 +0900

    Add Guid Integration Test

---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Models/GuidObjectModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp.Models
+{
+    public class GuidObjectModel
+    {
+        public Guid GuidValue { get; set; }
+    }
+}
