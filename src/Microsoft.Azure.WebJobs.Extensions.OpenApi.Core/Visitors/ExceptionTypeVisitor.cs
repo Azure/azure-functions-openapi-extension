93c71d35cf5ea0cfd88bd3f747b20a225e2d9200
level120 <tnara9573@gmail.com>
2021-10-11 21:16:03 +0900

    Remove property process for visit

--- src/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core/Visitors/ExceptionTypeVisitor.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core/Visitors/ExceptionTypeVisitor.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 using Microsoft.OpenApi.Models;
 
-using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Visitors
@@ -17,21 +14,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Visitors
     /// </summary>
     public class ExceptionTypeVisitor : TypeVisitor
     {
-        private readonly HashSet<string> _noAddedKeys = new HashSet<string>
-        {
-            "OBJECT",
-            "JTOKEN",
-            "JOBJECT",
-            "JARRAY",
-        };
-
-        private readonly HashSet<string> _serializePropertyNameSet = new HashSet<string>
-        {
-            nameof(Exception.InnerException),
-            nameof(Exception.Message),
-            nameof(Exception.StackTrace),
-        };
-
         /// <inheritdoc />
         public ExceptionTypeVisitor(VisitorCollection visitorCollection)
             : base(visitorCollection)
@@ -63,16 +45,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Visitors
                 return;
             }
 
-            // Processes properties.
-            var properties = type.Value
-                                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                 .Where(p => !p.ExistsCustomAttribute<JsonIgnoreAttribute>())
-                                 .Where(p => p.PropertyType != type.Value)
-                                 .Where(p => this._serializePropertyNameSet.Contains(p.Name))
-                                 .ToDictionary(p => p.GetJsonPropertyName(namingStrategy), p => p);
-
-            this.ProcessProperties(instance, name, properties, namingStrategy);
-
             // Adds the reference.
             var reference = new OpenApiReference()
             {
@@ -89,71 +61,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Visitors
             var isVisitable = this.IsVisitable(type);
 
             return isVisitable;
-        }
-
-        private void ProcessProperties(IOpenApiSchemaAcceptor instance, string schemaName, Dictionary<string, PropertyInfo> properties, NamingStrategy namingStrategy)
-        {
-            var schemas = new Dictionary<string, OpenApiSchema>();
-
-            var subAcceptor = new OpenApiSchemaAcceptor()
-            {
-                Properties = properties,
-                RootSchemas = instance.RootSchemas,
-                Schemas = schemas,
-            };
-
-            subAcceptor.Accept(this.VisitorCollection, namingStrategy);
-
-            // Add required properties to schema.
-            var jsonPropertyAttributes = properties.Where(p => !p.Value.GetCustomAttribute<JsonPropertyAttribute>(inherit: false).IsNullOrDefault())
-                                                   .Select(p => new KeyValuePair<string, JsonPropertyAttribute>(p.Key, p.Value.GetCustomAttribute<JsonPropertyAttribute>(inherit: false)))
-                                                   .Where(p => p.Value.Required == Required.Always || p.Value.Required == Required.DisallowNull);
-            foreach (var attribute in jsonPropertyAttributes)
-            {
-                instance.Schemas[schemaName].Required.Add(attribute.Key);
-            }
-
-            var jsonRequiredAttributes = properties.Where(p => !p.Value.GetCustomAttribute<JsonRequiredAttribute>(inherit: false).IsNullOrDefault())
-                                                   .Select(p => new KeyValuePair<string, JsonRequiredAttribute>(p.Key, p.Value.GetCustomAttribute<JsonRequiredAttribute>(inherit: false)));
-            foreach (var attribute in jsonRequiredAttributes)
-            {
-                var attributeName = namingStrategy.GetPropertyName(attribute.Key, hasSpecifiedName: false);
-                if (instance.Schemas[schemaName].Required.Contains(attributeName))
-                {
-                    continue;
-                }
-
-                instance.Schemas[schemaName].Required.Add(attributeName);
-            }
-
-            instance.Schemas[schemaName].Properties = subAcceptor.Schemas;
-
-            // Adds schemas to the root.
-            var schemasToBeAdded = subAcceptor.Schemas
-                                              .Where(p => !instance.Schemas.Keys.Contains(p.Key))
-                                              .Where(p => p.Value.IsOpenApiSchemaObject())
-                                              .ToDictionary(p => p.Value.Title, p => p.Value);
-
-            foreach (var schema in schemasToBeAdded.Where(p => !this._noAddedKeys.Contains(p.Key.ToUpperInvariant())))
-            {
-                if (instance.RootSchemas.ContainsKey(schema.Key))
-                {
-                    continue;
-                }
-
-                instance.RootSchemas.Add(schema.Key, schema.Value);
-            }
-
-            // Removes title of each property.
-            var subSchemas = instance.Schemas[schemaName].Properties;
-            subSchemas = subSchemas.Select(p =>
-                                    {
-                                        p.Value.Title = null;
-                                        return new KeyValuePair<string, OpenApiSchema>(p.Key, p.Value);
-                                    })
-                                   .ToDictionary(p => p.Key, p => p.Value);
-
-            instance.Schemas[schemaName].Properties = subSchemas;
         }
     }
 }
