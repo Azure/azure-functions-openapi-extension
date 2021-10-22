62a931f4cf531ea946bdc6caff16a74f556ab9b5
Greg <gsteen@packageaware.io>
2021-10-22 12:24:58 -0400

    fix tests

--- test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests/Configurations/OpenApiSettingsTests.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests/Configurations/OpenApiSettingsTests.cs
@@ -15,9 +15,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Tests.Configurations
     public class OpenApiSettingsTests
     {
         [DataTestMethod]
-        [DataRow("true", "false", true, false, "lorem", "Function", AuthorizationLevel.Function, "Anonymous", AuthorizationLevel.Anonymous, "http://localhost:7071", "https://contoso.com/api/")]
-        [DataRow("false", "false", false, false, "ipsum", "Anonymous", AuthorizationLevel.Anonymous, "Function", AuthorizationLevel.Function, "http://contoso", "https://fabrikam.com/api/")]
-        [DataRow("false", "true", false, true, "ipsum", "Anonymous", AuthorizationLevel.Anonymous, "Function", AuthorizationLevel.Function, "http://contoso", "https://fabrikam.com/api/")]
+        [DataRow("true", true, "false", false, "lorem", "Function", AuthorizationLevel.Function, "Anonymous", AuthorizationLevel.Anonymous, "http://localhost:7071", "https://contoso.com/api/")]
+        [DataRow("false", false, "false", false, "ipsum", "Anonymous", AuthorizationLevel.Anonymous, "Function", AuthorizationLevel.Function, "http://contoso", "https://fabrikam.com/api/")]
+        [DataRow("false", false, "true", true, "ipsum", "Anonymous", AuthorizationLevel.Anonymous, "Function", AuthorizationLevel.Function, "http://contoso", "https://fabrikam.com/api/")]
         public void Given_EnvironmentVariables_When_Instantiated_Then_It_Should_Return_Result(string hideSwaggerUI, bool expectedHideSwaggerUI,
                                                                                               string hideDocument, bool expectedHideDocument,
                                                                                               string apiKey,
