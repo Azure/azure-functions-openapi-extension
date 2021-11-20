b75655c925069f867027954b11deea81931d6b6e
lee-ji-an <leeja0407@naver.com>
2021-11-21 01:36:58 +0900

    add array trigger and test

---
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.Document.Tests/Get_ApplicationJson_Array_Tests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Document.Tests
+{
+    [TestClass]
+    [TestCategory(Constants.TestCategory)]
+    public class Get_ApplicationJson_Array_Tests
+    {
+        private static HttpClient http = new HttpClient();
+
+        private JObject _doc;
+
+        [TestInitialize]
+        public async Task Init()
+        {
+            var json = await http.GetStringAsync(Constants.OpenApiDocEndpoint).ConfigureAwait(false);
+            this._doc = JsonConvert.DeserializeObject<JObject>(json);
+        }
+
+        [DataTestMethod]
+        [DataRow("/get-applicationjson-string-array")]
+        public void Given_OpenApiDocument_Then_It_Should_Return_Path(string path)
+        {
+            var paths = this._doc["paths"];
+
+            paths.Value<JToken>(path).Should().NotBeNull();
+        }
+
+        
+    }
+}
