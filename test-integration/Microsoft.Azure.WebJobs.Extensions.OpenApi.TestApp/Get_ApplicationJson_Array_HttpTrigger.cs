df490f2019cd7fd1fc17457a604d3b39c3f12761
lee-ji-an <leeja0407@naver.com>
2021-11-21 10:59:45 +0900

    Change tags name of array endpoint

--- test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Get_ApplicationJson_Array_HttpTrigger.cs
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp/Get_ApplicationJson_Array_HttpTrigger.cs
@@ -16,7 +16,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp
     {
         
         [FunctionName(nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_StringArray))]
-        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_StringArray), tags: new[] { "string-array" })]
+        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_StringArray), tags: new[] { "array" })]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string[]), Description = "The OK response")]
         public static async Task<IActionResult> Get_ApplicationJson_StringArray(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "get-applicationjson-string-array")] HttpRequest req,
@@ -28,7 +28,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp
         }
 
         [FunctionName(nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntArray))]
-        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntArray), tags: new[] { "int-array" })]
+        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntArray), tags: new[] { "array" })]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(int[]), Description = "The OK response")]
         public static async Task<IActionResult> Get_ApplicationJson_IntArray(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "get-applicationjson-int-array")] HttpRequest req,
@@ -40,7 +40,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp
         }
 
         [FunctionName(nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_BoolArray))]
-        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_BoolArray), tags: new[] { "bool-array" })]
+        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_BoolArray), tags: new[] { "array" })]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(bool[]), Description = "The OK response")]
         public static async Task<IActionResult> Get_ApplicationJson_BoolArray(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "get-applicationjson-bool-array")] HttpRequest req,
@@ -52,7 +52,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.TestApp
         }
 
         [FunctionName(nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntList))]
-        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntList), tags: new[] { "int-list" })]
+        [OpenApiOperation(operationId: nameof(Get_ApplicationJson_Array_HttpTrigger.Get_ApplicationJson_IntList), tags: new[] { "array" })]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<int>), Description = "The OK response")]
         public static async Task<IActionResult> Get_ApplicationJson_IntList(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "get-applicationjson-int-list")] HttpRequest req,
