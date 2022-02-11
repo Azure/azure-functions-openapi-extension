20fa604ed275a22aee04e1b87eaaffd71256a119
Justin Yoo <justin.yoojh@gmail.com>
2022-02-11 16:32:16 +0900

    Update CustomUIOptions default value

--- samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.InProc/Configurations/OpenApiCustomUIOptions.cs
+++ samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.InProc/Configurations/OpenApiCustomUIOptions.cs
@@ -11,8 +11,14 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.InProc.Configur
         {
         }
 
-        public override string CustomStylesheetPath { get; } = "dist.my-custom.css";
+        //<!-- Uncomment if you want to use the embedded file. -->
+        //public override string CustomStylesheetPath { get; } = "dist.my-custom.css";
+        //public override string CustomJavaScriptPath { get; } = "dist.my-custom.js";
+        //<!-- Uncomment if you want to use the embedded file. -->
 
-        public override string CustomJavaScriptPath { get; } = "dist.my-custom.js";
+        //<!-- Uncomment if you want to use the external URL. -->
+        //public override string CustomStylesheetPath { get; } = "https://raw.githubusercontent.com/Azure/azure-functions-openapi-extension/main/samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.InProc/dist/my-custom.css";
+        //public override string CustomJavaScriptPath { get; } = "https://raw.githubusercontent.com/Azure/azure-functions-openapi-extension/main/samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.InProc/dist/my-custom.js";
+        //<!-- Uncomment if you want to use the external URL. -->
     }
 }
