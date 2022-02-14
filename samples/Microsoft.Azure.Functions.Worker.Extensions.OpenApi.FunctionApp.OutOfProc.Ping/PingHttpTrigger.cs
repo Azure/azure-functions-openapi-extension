cde3b5512ad3b7fc6640b9ec58559e79a71dcba8
Justin Yoo <justin.yoojh@gmail.com>
2022-02-14 11:37:15 +0900

    Add method to load types from referenced function app

--- samples/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.OutOfProc.Ping/PingHttpTrigger.cs
+++ samples/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.OutOfProc.Ping/PingHttpTrigger.cs
@@ -19,11 +19,14 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.OpenApi.FunctionApp.OutOfP
 
         [Function(nameof(PingHttpTrigger.Ping))]
         [OpenApiOperation(operationId: "ping", tags: new[] { "ping" }, Summary = "Pings for health check", Description = "This pings for health check.", Visibility = OpenApiVisibilityType.Important)]
-        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK, Summary = "Successful operation", Description = "Successful operation")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "Successful operation", Description = "Successful operation")]
         public async Task<HttpResponseData> Ping(
             [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "ping")] HttpRequestData req)
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            await response.WriteStringAsync("pong").ConfigureAwait(false);
 
             return await Task.FromResult(response).ConfigureAwait(false);
         }
