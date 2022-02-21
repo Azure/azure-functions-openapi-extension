f2ffe91e692bedbc8da9b7ed876dfcef58d495c7
Greg <gsteen@packageaware.io>
2022-02-21 16:56:48 -0500

    remove comment

--- test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Fakes/FakeOpenApiConfigurationOptionsBase.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Fakes/FakeOpenApiConfigurationOptionsBase.cs
@@ -2,7 +2,6 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
 
 namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Fakes
 {
-    // NOTE: this abstract class is for testing the OpenApiConfigurationResolver.Resolve method (and will be ignored)
     public abstract class FakeOpenApiConfigurationOptionsBase : DefaultOpenApiConfigurationOptions
     {
     }
