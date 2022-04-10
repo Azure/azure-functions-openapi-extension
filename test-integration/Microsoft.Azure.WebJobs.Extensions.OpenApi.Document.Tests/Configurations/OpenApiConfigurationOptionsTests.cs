e7ac503fde8442b547bb8598f27637bef2e62a19
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2022-04-10 13:26:05 +0900

    Remove unrelated code

--- test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.Document.Tests/Configurations/OpenApiConfigurationOptionsTests.cs
+++ test-integration/Microsoft.Azure.WebJobs.Extensions.OpenApi.Document.Tests/Configurations/OpenApiConfigurationOptionsTests.cs
@@ -26,71 +26,13 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Document.Tests.OpenApiInfo
             this._json = await http.GetStringAsync(Constants.OpenApiDocEndpoint).ConfigureAwait(false);
             this._doc = JsonConvert.DeserializeObject<OpenApiDocument>(this._json);
         }
-        
+
         [TestMethod]
         public void Given_OpenApiDocument_When_ForceHttps_Given_Then_It_Should_Return_Https()
         {
             var servers = this._doc.Servers;
 
             servers[0].Url.Should().Be("https://localhost:7071/api");
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_Title()
-        {
-            this._doc.Info.Title.Should().Be(OpenApiInfoConfigs.DocTitle);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_Description()
-        {
-            this._doc.Info.Description.Should().Be(OpenApiInfoConfigs.DocDescription);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_TermsOfService()
-        {
-            this._doc.Info.TermsOfService.AbsoluteUri.Should().Be(OpenApiInfoConfigs.TermsOfService);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_ContactName()
-        {
-            var contact = this._doc.Info.Contact;
-
-            contact.Name.Should().Be(OpenApiInfoConfigs.ContactName);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_ContactEmail()
-        {
-            var contact = this._doc.Info.Contact;
-
-            contact.Email.Should().Be(OpenApiInfoConfigs.ContactEmail);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_ContactUrl()
-        {
-            var contact = this._doc.Info.Contact;
-
-            contact.Url.AbsoluteUri.Should().Be(OpenApiInfoConfigs.ContactUrl);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_LicenseName()
-        {
-            var license = this._doc.Info.License;
-
-            license.Name.Should().Be(OpenApiInfoConfigs.LicenseName);
-        }
-
-        [TestMethod]
-        public void Given_OpenApiDocument_Then_It_Should_Return_LicenseUrl()
-        {
-            var license = this._doc.Info.License;
-
-            license.Url.AbsoluteUri.Should().Be(OpenApiInfoConfigs.LicenseUrl);
         }
     }
 }
