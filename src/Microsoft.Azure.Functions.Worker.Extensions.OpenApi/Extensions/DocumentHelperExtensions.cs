58ceb8c8cfdeca23493e89253af521a0f7b2ebf9
Demetrius Axenowski <d.axenowski@ax-soft.de>
2022-04-26 15:26:44 +0200

    This fixes issue #424

--- src/Microsoft.Azure.Functions.Worker.Extensions.OpenApi/Extensions/DocumentHelperExtensions.cs
+++ src/Microsoft.Azure.Functions.Worker.Extensions.OpenApi/Extensions/DocumentHelperExtensions.cs
@@ -21,27 +21,6 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions
     /// </summary>
     public static class DocumentHelperExtensions
     {
-
-        /// <summary>
-        /// Distinct by defined property
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="keySelector"></param>
-        /// <typeparam name="TSource"></typeparam>
-        /// <typeparam name="TKey"></typeparam>
-        /// <returns></returns>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-        {
-            var seenKeys = new HashSet<TKey>();
-            foreach (var element in source)
-            {
-                if (seenKeys.Add(keySelector(element)))
-                {
-                    yield return element;
-                }
-            }
-        }
-
         /// <summary>
         /// Gets the list of HTTP triggers.
         /// </summary>
