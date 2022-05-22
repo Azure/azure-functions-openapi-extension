4bd308ec50765bb95bffe13e453a44bf43919760
did7121 <85928740+did7121@users.noreply.github.com>
2022-05-22 14:14:28 +0900

    add DocumentTests (#442)

--- test/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Tests/DocumentTests.cs
+++ test/Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Tests/DocumentTests.cs
@@ -558,5 +558,36 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Tests
             ((string)json?.basePath).Should().BeEquivalentTo(null);
             ((string)json?.schemes[0]).Should().BeEquivalentTo(scheme);
         }
+
+        [DataTestMethod]
+        [DataRow("https", "localhost", "api")]
+        [DataRow("https", "localhost", "")]
+        [DataRow("https", "localhost", null)]
+        public async Task Given_ServerDetails_When_RenderAsync_Invoked_Then_It_Should_Return_Result(string scheme, string host, string routePrefix)
+        {
+            var helper = new Mock<IDocumentHelper>();
+
+            var url = $"{scheme}://{host}/{routePrefix}";
+            var uri = new Uri(url);
+            var req = new Mock<IHttpRequestDataObject>();
+            req.SetupGet(p => p.Scheme).Returns(scheme);
+
+            req.SetupGet(p => p.Host).Returns(new HostString(host));
+
+
+            var doc = new Document(helper.Object);
+
+            var result = await doc.InitialiseDocument()
+                                  .AddServer(req.Object, routePrefix)
+                                  .RenderAsync(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json);
+
+            dynamic json = JObject.Parse(result);
+
+            ((string)json?.host).Should().BeEquivalentTo(null);
+            ((string)json?.basePath).Should().BeEquivalentTo(null);
+            ((string)json?.schemes).Should().BeEquivalentTo(null);
+            ((string)json?.servers[0].url).Should().BeEquivalentTo(uri.AbsoluteUri);
+        }
     }
 }
+
