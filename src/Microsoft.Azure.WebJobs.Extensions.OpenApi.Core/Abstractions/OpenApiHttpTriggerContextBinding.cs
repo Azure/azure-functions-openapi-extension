0f3e3b6e6bfaaee18c140f755e9ebda6cc3dc7f6
Jack Batzner <jackbatzner@microsoft.com>
2022-08-02 13:59:37 -0500

    Update comment

--- src/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core/Abstractions/OpenApiHttpTriggerContextBinding.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core/Abstractions/OpenApiHttpTriggerContextBinding.cs
@@ -8,19 +8,19 @@ using Newtonsoft.Json;
 namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions
 {
     /// <summary>
-    /// This represents the metadata entity for HTTP trigger context binding.
+    /// This represents the metadata entity for OpenApiHttpTrigger context binding.
     /// </summary>
     public class OpenApiHttpTriggerContextBindingMetadata
     {
         /// <summary>
-        /// Gets or sets the name of the binding parameter. Default value is <c>req</c>.
+        /// Gets or sets the name of the binding parameter. Default value is <c>context</c>.
         /// </summary>
         [JsonRequired]
         [JsonProperty("name")]
         public virtual string Name { get; set; } = "context";
 
         /// <summary>
-        /// Gets or sets the binding type. Default value is <c>httpTriggerContext</c>.
+        /// Gets or sets the binding type. Default value is <c>openApiHttpTriggerContext</c>.
         /// </summary>
         [JsonRequired]
         [JsonProperty("type")]
