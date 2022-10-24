50e4574ba4eaaa75632cb261d9d3f2a26478ba67
Justin Yoo <justin.yoojh@gmail.com>
2022-10-24 07:04:37 +0000

    Update OpenAPI doc script

--- actions/get-openapi-document.sh
+++ actions/get-openapi-document.sh
@@ -12,10 +12,6 @@ function usage() {
                                 Default: 'http://localhost:7071/api/'
         [-e|--endpoint]         OpenAPI document endpoint.
                                 Default: 'swagger.json'
-        [-o|--output-path]      Output directory to store the generated OpenAPI document, relative to the repository root.
-                                Default: 'generated'
-        [-f|--output-filename]  Output filename for the generated OpenAPI document.
-                                Default: 'swagger.json'
         [-d|--delay]            Delay in second between the function app run and document generation.
                                 Default: 30
         [-c|--use-codespaces]   Switch indicating whether to use GitHub Codespaces or not.
@@ -28,8 +24,6 @@ USAGE
 functionapp_path="."
 base_uri="http://localhost:7071/api/"
 endpoint="swagger.json"
-output_path="generated"
-output_filename="swagger.json"
 delay=30
 repository_root=$GITHUB_WORKSPACE
 
@@ -37,8 +31,6 @@ if [[ $# -eq 0 ]]; then
     functionapp_path="."
     base_uri="http://localhost:7071/api/"
     endpoint="swagger.json"
-    output_path="generated"
-    output_filename="swagger.json"
     delay=30
     repository_root=$GITHUB_WORKSPACE
 fi
@@ -58,16 +50,6 @@ while [[ "$1" != "" ]]; do
     -e | --endpoint)
         shift
         endpoint=$1
-        ;;
-
-    -o | --output-path)
-        shift
-        output_path=$1
-        ;;
-
-    -f | --output-filename)
-        shift
-        output_filename=$1
         ;;
 
     -d | --delay)
@@ -103,14 +85,9 @@ func start --verbose false &
 sleep $delay
 
 request_uri="$(echo "$base_uri" | sed 's:/*$::')/$(echo "$endpoint" | sed 's:^/*::')"
-filepath="$repository_root/$(echo "$output_path" | sed 's:/*$::')/$(echo "$output_filename" | sed 's:^/*::')"
-
-if [ ! -d "$repository_root/$output_path" ]; then
-    mkdir "$repository_root/$output_path"
-fi
 
 # Download the OpenAPI document
-curl $request_uri > $filepath
+openapi=$(curl $request_uri)
 
 # Stop the function app
 PID=$(lsof -nP -i4TCP:7071 | grep LISTEN | awk '{print $2}')
@@ -119,3 +96,5 @@ if [[ "" !=  "$PID" ]]; then
 fi
 
 cd $current_directory
+
+echo $openapi
