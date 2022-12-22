b55647c1c2cfd6c050d428c9747c0de4f3eb2e64
Justin Yoo <justin.yoojh@gmail.com>
2022-12-22 15:54:39 +0900

    Update OpenApiSettings (#526)

--- src/Microsoft.Azure.WebJobs.Extensions.OpenApi/OpenApiWebJobsStartup.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.OpenApi/OpenApiWebJobsStartup.cs
@@ -1,9 +1,9 @@
 using Microsoft.Azure.WebJobs.Extensions.OpenApi;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Extensions;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Resolvers;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Extensions.DependencyInjection;
---
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests/Configurations/OpenApiAuthLevelSettingsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Configurations.AppSettings.Resolvers;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Configurations
+{
+    [TestClass]
+    public class OpenApiAuthLevelSettingsTests
+    {
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.SetEnvironmentVariable("OpenApi__AuthLevel__Document", null);
+            Environment.SetEnvironmentVariable("OpenApi__AuthLevel__UI", null);
+        }
+
+        [DataTestMethod]
+        [DataRow(null, null)]
+        [DataRow("", null)]
+        [DataRow("Anonymous", OpenApiAuthLevelType.Anonymous)]
+        [DataRow("anonymous", OpenApiAuthLevelType.Anonymous)]
+        [DataRow("Function", OpenApiAuthLevelType.Function)]
+        [DataRow("function", OpenApiAuthLevelType.Function)]
+        [DataRow("User", OpenApiAuthLevelType.User)]
+        [DataRow("user", OpenApiAuthLevelType.User)]
+        [DataRow("Admin", OpenApiAuthLevelType.Admin)]
+        [DataRow("admin", OpenApiAuthLevelType.Admin)]
+        [DataRow("System", OpenApiAuthLevelType.System)]
+        [DataRow("system", OpenApiAuthLevelType.System)]
+        public void Given_AuthLevelDoc_When_Instantiated_Then_It_Should_Return_Result(string authLevel, OpenApiAuthLevelType? expected)
+        {
+            Environment.SetEnvironmentVariable("OpenApi__AuthLevel__Document", authLevel);
+
+            var config = ConfigurationResolver.Resolve();
+            var settings = config.Get<OpenApiSettings>(OpenApiSettings.Name);
+            var authlevel = settings.AuthLevel;
+
+            authlevel.Document.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(null, null)]
+        [DataRow("", null)]
+        [DataRow("Anonymous", OpenApiAuthLevelType.Anonymous)]
+        [DataRow("anonymous", OpenApiAuthLevelType.Anonymous)]
+        [DataRow("Function", OpenApiAuthLevelType.Function)]
+        [DataRow("function", OpenApiAuthLevelType.Function)]
+        [DataRow("User", OpenApiAuthLevelType.User)]
+        [DataRow("user", OpenApiAuthLevelType.User)]
+        [DataRow("Admin", OpenApiAuthLevelType.Admin)]
+        [DataRow("admin", OpenApiAuthLevelType.Admin)]
+        [DataRow("System", OpenApiAuthLevelType.System)]
+        [DataRow("system", OpenApiAuthLevelType.System)]
+        public void Given_AuthLevelUI_When_Instantiated_Then_It_Should_Return_Result(string authLevel, OpenApiAuthLevelType? expected)
+        {
+            Environment.SetEnvironmentVariable("OpenApi__AuthLevel__UI", authLevel);
+
+            var config = ConfigurationResolver.Resolve();
+            var settings = config.Get<OpenApiSettings>(OpenApiSettings.Name);
+            var authlevel = settings.AuthLevel;
+
+            authlevel.UI.Should().Be(expected);
+        }
+    }
+}
