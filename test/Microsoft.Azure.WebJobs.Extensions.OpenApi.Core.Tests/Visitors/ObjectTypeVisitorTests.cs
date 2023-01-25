50d854a84ab590e45e52906f747f084192563552
William-Froelich <falash@gmail.com>
2023-01-25 15:55:12 -0600

    Add RecursiveObjectTypeVisitor test

--- test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests/Visitors/ObjectTypeVisitorTests.cs
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests/Visitors/ObjectTypeVisitorTests.cs
@@ -87,7 +87,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Visitors
         [DataTestMethod]
         [DataRow(typeof(FakeModel), "object", null, 3, 3, "fakeModel")]
         [DataRow(typeof(FakeRequiredModel), "object", null, 1, 0, "fakeRequiredModel")]
-        [DataRow(typeof(FakeRecursiveModel), "object", null, 1, 2, "fakeRecursiveModel")]
+        [DataRow(typeof(FakeRecursiveModel), "object", null, 3, 2, "fakeRecursiveModel")]
         public void Given_Type_When_Visit_Invoked_Then_It_Should_Return_Result(Type objectType, string dataType, string dataFormat, int requiredCount, int rootSchemaCount, string referenceId)
         {
             var name = "hello";
