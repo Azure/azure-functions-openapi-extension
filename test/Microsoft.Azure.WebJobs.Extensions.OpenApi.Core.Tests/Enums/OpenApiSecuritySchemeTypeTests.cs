656b901e595d5c45b02a6792138fbe564ab26581
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-05-14 11:56:46 +0900

    Add  security scheme type test (#579)

---
+++ test/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests/Enums/OpenApiSecuritySchemeTypeTests.cs
@@ -0,0 +1,53 @@
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Tests.Enums
+{
+    [TestClass]
+    public class OpenApiSecuritySchemeTypeTests
+    {
+        [DataTestMethod]
+        [DataRow("None")]
+        [DataRow("Basic")]
+        [DataRow("Bearer")]
+        public void Given_Enum_Should_Have_Member(string memberName)
+        {
+            var members = typeof(OpenApiSecuritySchemeType).GetMembers().Select(p => p.Name);
+
+            members.Should().Contain(memberName);
+        }
+
+        [DataTestMethod]
+        [DataRow("Basic", "basic")]
+        [DataRow("Bearer", "bearer")]
+        public void Given_Enum_Should_Have_Decorator(string memberName, string displayName)
+        {
+            var member = this.GetMemberInfo(memberName);
+            var attribute = member.GetCustomAttribute<DisplayAttribute>(inherit: false);
+
+            attribute.Should().NotBeNull();
+            attribute.Name.Should().Be(displayName);
+        }
+
+        [DataTestMethod]
+        [DataRow("None", null)]
+        public void Given_Enum_Should_Have_No_Decorator(string memberName, string displayName)
+        {
+            var member = this.GetMemberInfo(memberName);
+            var attribute = member.GetCustomAttribute<DisplayAttribute>(inherit: false);
+
+            attribute.Should().BeNull();
+        }
+
+        private MemberInfo GetMemberInfo(string name)
+        {
+            var member = typeof(OpenApiSecuritySchemeType).GetMember(name).First();
+
+            return member;
+        }
+    }
+}
