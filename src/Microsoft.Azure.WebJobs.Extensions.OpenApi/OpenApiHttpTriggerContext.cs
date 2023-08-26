06136532713aba3b3267858543e09e1e3ef6dc7f
songseungwoon <andrew960811@naver.com>
2023-08-26 20:47:47 +0900

    FEAT: NamingStrategy in Worker Extensions

--- src/Microsoft.Azure.WebJobs.Extensions.OpenApi/OpenApiHttpTriggerContext.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.OpenApi/OpenApiHttpTriggerContext.cs
@@ -128,7 +128,24 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi
         public virtual ISwaggerUI SwaggerUI { get; }
 
         /// <inheritdoc />
-        public virtual NamingStrategy NamingStrategy { get; } = new CamelCaseNamingStrategy();
+        public virtual NamingStrategy NamingStrategy { 
+            get
+            { 
+                switch (this._configOptions.NamingStrategy)
+                {
+                    case NamingStrategyType.CamelCase:
+                        return new CamelCaseNamingStrategy();
+                    case NamingStrategyType.PascalCase:
+                        return new DefaultNamingStrategy();
+                    case NamingStrategyType.SnakeCase:
+                        return new SnakeCaseNamingStrategy();
+                    case NamingStrategyType.KebabCase:
+                        return new KebabCaseNamingStrategy();
+                    default:
+                        return new CamelCaseNamingStrategy();
+                }
+            }
+        }
 
         /// <inheritdoc />
         public virtual bool IsDevelopment { get; } = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development";
