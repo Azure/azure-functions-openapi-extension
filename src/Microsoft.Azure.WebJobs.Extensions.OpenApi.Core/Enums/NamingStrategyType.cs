44cacfe4588f764c6285759438de32da4f4097d2
hhiyeon <hhyyeon0214@gmail.com>
2023-08-29 14:38:52 +0900

    feat: create enum type for naming strategy

---
+++ src/Microsoft.Azure.WebJobs.Extensions.OpenApi.Core/Enums/NamingStrategyType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums
+{
+    public enum NamingStrategyType
+    {
+        CamelCase,
+        PascalCase,
+        SnakeCase,
+        KebabCase
+    }
+}
